cf3e733e290f77f07195b87d957d014f2af697f8
Preeti Yuankrathok <preetisatit@gmail.com>
2018-05-02 04:10:24 +0700

    success for 1

--- segment.py
+++ segment.py
@@ -1,11 +1,13 @@
 import json
 
-input_file = open('panther.txt').read().translate({ord(c): ' ' for c in '!@#$%^&*()[]{};:,./<>?\|`~-=_+'})
+file_name = 'panther_test.txt'
+input_file = open(file_name).read().translate({ord(c): ' ' for c in '!@#$%^&*()[]{};:,./<>?\|`~-=_+"'})
 word_list = json.load(open('thai-wordlist.json'))
 
-words = {}
+words = []
 current = ''
 last_match = ''
+ever_left = False
 pos = 0
 last_pos = 0
 
@@ -16,15 +18,25 @@ def word_left(str):
             n += 1
     return n
 
-def save():
-    words[pos] = {}
-    current = ''
-    last_match = ''
-    last_pos = pos
+def check(str):
+    return str in word_list
 
-for i, c in enumerate(input_file):
-    if c == ' ':
-        save()
+i = -1
+while i < len(input_file) - 1:
+    i += 1
+    c = input_file[i]
+    if c == ' ' and (len(words) == 0 or pos + len(last_match) > words[-1]['pos'] + len(words[-1]['word'])):
+        if current in word_list:
+            words.append({'pos': pos, 'word': last_match})
+            i = words[-1]['pos']
+            last_pos = pos
+        current = ''
+        last_match = ''
+        continue
+    elif c == len(input_file) - 1 and (len(words) == 0 or pos + len(last_match) > words[-1]['pos'] + len(words[-1]['word'])):
+        words.append({'pos': pos, 'word': last_match})
+        last_pos = pos
+        break
     elif ord(c) < 3585 or ord(c) > 3662:
         continue
 
@@ -33,14 +45,35 @@ for i, c in enumerate(input_file):
 
     current = current + c
     left = word_left(current)
+    if (left > 0):
+        ever_left = True
 
-    # print(current, left)
+    # print(i, current, left)
     # if i == 10:
     #     break
 
-    if current in word_list:
+    if check(current):
         last_match = current
         if left == 1:
-            save()
+            if len(words) == 0 or pos + len(last_match) > words[-1]['pos'] + len(words[-1]['word']):
+                words.append({'pos': pos, 'word': last_match})
+                i = words[-1]['pos']
+                current = ''
+                last_match = ''
+                last_pos = pos
+            continue
+    
+    if left == 0:
+        if last_match != '':
+            if len(words) == 0 or pos + len(last_match) > words[-1]['pos'] + len(words[-1]['word']):
+                words.append({'pos': pos, 'word': last_match})
+                last_pos = pos
+                i = words[-1]['pos']
+            else:
+                i = pos
+        elif ever_left:
+            i = pos
+        current = ''
+        last_match = ''
 
 print(words)
