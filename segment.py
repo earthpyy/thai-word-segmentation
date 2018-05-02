d68560f80d30367f42c8bb27f029c906f00f2338
Preeti Yuankrathok <preetisatit@gmail.com>
2018-05-02 07:47:42 +0700

    step 3 done!

--- segment.py
+++ segment.py
@@ -30,10 +30,11 @@ while i < len(input_file) - 1:
             words.append({'valid': True, 'pos': pos, 'word': last_match, 'adj': []})
             i = words[-1]['pos']
             last_pos = pos
-        else:
-            words.append({'valid': False, 'pos': pos, 'word': current})
+        # else:
+        #     words.append({'valid': False, 'pos': pos, 'word': current})
         current = ''
         last_match = ''
+        ever_left = False
         continue
     # elif c == len(input_file) - 1 and (len(words) == 0 or pos + len(last_match) > words[-1]['pos'] + len(words[-1]['word'])):
     #     words.append({'valid': True, 'pos': pos, 'word': last_match, 'adj': []})
@@ -50,9 +51,14 @@ while i < len(input_file) - 1:
     if (left > 0):
         ever_left = True
 
-    # print(i, current, left)
-    # if i == 10:
-    #     break
+    if c == len(input_file) - 1:
+        if check(current) and (len(words) == 0 or pos + len(current) > words[-1]['pos'] + len(words[-1]['word'])):
+            words.append({'valid': True, 'pos': pos, 'word': current, 'adj': []})
+        elif len(words) == 0 or pos + len(last_match) > words[-1]['pos'] + len(words[-1]['word']):
+            words.append({'valid': True, 'pos': pos, 'word': last_match, 'adj': []})
+        # TODO: valid False condition
+        last_pos = pos
+        break
 
     if check(current):
         last_match = current
@@ -63,6 +69,7 @@ while i < len(input_file) - 1:
                 current = ''
                 last_match = ''
                 last_pos = pos
+                ever_left = False
             continue
     
     if left == 0:
@@ -75,10 +82,17 @@ while i < len(input_file) - 1:
                 i = pos
         elif ever_left:
             i = pos
+        # else:
+        #     words.append({'valid': False, 'pos': pos, 'word': current})
+
         current = ''
         last_match = ''
+        ever_left = False
 
 for index, dic in enumerate(words):
+    if not dic['valid']:
+        continue
+
     last_k = len(words) - index
     if last_k > 5:
         max_k = 5
@@ -88,14 +102,20 @@ for index, dic in enumerate(words):
         max_k = last_k
 
     dic_len = len(dic['word'])
+    min_dis = 2147483647
+    min_dis_i = None
 
     for k in range(1, max_k):
-        if not words[index + k]['valid']:
-            if (max_k < last_k):
-                max_k += 1
-            continue
+        # if not words[index + k]['valid']:
+        #     if (max_k < last_k):
+        #         max_k += 1
+        #     continue
         
-        if dic['pos'] + dic_len == words[index + k]['pos']:
+        distance = words[index + k]['pos'] - dic['pos'] - dic_len
+        if distance < min_dis:
+            min_dis_i = k
+            min_dis = distance
+        if distance == 0:
             words[index]['adj'].append({'index': index + k, 'val': 1})
 
         i = 0
@@ -155,5 +175,42 @@ for index, dic in enumerate(words):
                         words[index]['adj'].append({'index': index + k, 'val': 1000, 'same': word_l})
 
             i += 1
+    
+    if (min_dis > 0 and min_dis_i != None):
+        unknown = ''
+        for i in range(dic['pos'] + dic_len, words[index + min_dis_i]['pos']):
+            unknown += input_file[i]
+        words[index]['adj'] = []
+        words.insert(index + 1, {'valid': False, 'pos': dic['pos'] + dic_len, 'word': unknown, 'adj': []})
 
-print(words)
+# step 3
+node = []
+for i, word in enumerate(words):
+    if word['valid']:
+        node.append(i)
+        break
+
+i = node[-1]
+while words[i] != words[-1]:
+    if not words[i]['adj']:
+        i += 1
+        while not words[i]['valid']:
+            i += 1
+        node.append(i)
+    else:
+        min_adj = 1001
+        min_adj_i = -1
+        for adj in words[i]['adj']:
+            if adj['val'] < min_adj:
+                min_adj = adj['val']
+                min_adj_i = adj['index']
+        node.append(min_adj_i)
+        i = min_adj_i
+
+# for nod in node:
+#     print(words[nod]['word'])
+
+# step 4
+
+# for i, word in enumerate(words):
+#     print(i, word['word'], word['adj'])
